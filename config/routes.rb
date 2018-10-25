4ecfaee5b232461986c0b7be19e9e3fb76851085
Paweł Nicewicz <nicewiczpawel@gmail.com>
2018-10-25 08:50:01 +0000

    admin redirects to 'users/sign_in'

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :events
   root 'homepage#index'
-  get 'admin'
+  as :user do
+    get '/admin', to: 'devise/sessions#new'
+  end
 end
