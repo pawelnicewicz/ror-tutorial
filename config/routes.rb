84f6d8c68600769dc99ab1ac672a9433ec5860ae
Paweł Nicewicz <nicewiczpawel@gmail.com>
2018-10-25 09:04:30 +0000

    redirect homepage and add scss

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :events
-  root 'homepage#index'
+  root 'events#index', as: :homepage
   as :user do
     get '/admin', to: 'devise/sessions#new'
   end
