fb6e44f5d0b6081319a9b473e862ff07c036e8c7
Paweł Nicewicz <nicewiczpawel@gmail.com>
2018-10-29 08:21:25 +0000

    homepage controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :events
-  root 'events#index', as: :homepage
+  root 'homepage#index'
   as :user do
     get '/admin', to: 'devise/sessions#new'
   end
